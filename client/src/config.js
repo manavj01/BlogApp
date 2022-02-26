c68f77c871b9f80f93d3d87b29485ad6150c73d9
Manav Jain <manavj0120@gmail.com>
2022-02-26 15:51:01 +0530

    updated clientId

--- client/src/config.js
+++ client/src/config.js
@@ -3,13 +3,13 @@ const oktaAuthConfig = {
   // instead of the Authorization Code with Proof of Code Key Exchange (PKCE)
   // you will need to add `pkce: false`
   issuer: 'https://dev-50475973.okta.com/oauth2/default',
-  clientId: '0oa252tvke9OzjxZX5d7',
+  clientId: '0oa3zms0qvrGwG7y15d7',
   redirectUri: window.location.origin + '/login/callback',
 };
 
 const oktaSignInConfig = {
   baseUrl: 'https://dev-50475973.okta.com',
-  clientId: '0oa252tvke9OzjxZX5d7',
+  clientId: '0oa3zms0qvrGwG7y15d7',
   redirectUri: window.location.origin + '/login/callback',
   authParams: {
     // If your app is configured to use the Implicit flow
